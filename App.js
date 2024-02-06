2339d38882eeb7bea9c991e686d950036557dadd
leandro farias <aguslean2008@hotmail.com>
2024-02-06 18:32:45 -0300

    segunda entrega

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import Constants from "expo-constants";
 import {
   Text,
   View,
@@ -7,11 +9,12 @@ import {
   StyleSheet,
   Pressable,
   FlatList,
-  Modal
 } from "react-native";
-import Logo from "./assets/Hoeklogo.png";
-
-const product = [
+import cartLogo from "./assets/Hoeklogo.png";
+import RemoveModal from "./src/components/RemoveModal";
+import lupa  from "./assets/lupa.png";
+import cruz from "./assets/cruz.png"
+const DATA = [
   {
     name: "Remera",
     id: 1,
@@ -27,86 +30,119 @@ const product = [
 ];
 
 export default function App() {
-  // useState y useEffect hooks para controlar el estado de la aplicación y el ciclo de vida de un componente
-  const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState("");
-  const [cartItems, setCartItems] = useState([])
-
-  const handleAddCounter = () => setCounter(counter + 1);
+  const [cartItems, setCartItems] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [itemSelected, setItemSelected] = useState(null);
 
   const handleInputChange = (value) => setInputValue(value);
+
+  const handleModal = (id) => {
+    setModalVisible(true);
+    setItemSelected(id);
+    console.log(id);
+  };
+
 
   const addItem = () => {
     const newItem = {
       name: inputValue,
-      id: new Date().getTime()
-    }
-    setCartItems([...cartItems, newItem])
-  }
+      id: new Date().getTime(),
+    };
+    setCartItems([...cartItems, newItem]);
+  };
 
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <RemoveModal
+        modalVisible={modalVisible}
+        cartItems={cartItems}
+        setCartItems={setCartItems}
+        setModalVisible={setModalVisible}
+        itemSelected={itemSelected}
+      />
+
       <View style={styles.header}>
-        <view><Text>TIENDA</Text></view>
-        
-        <view><Image style={styles.image} source={Logo} /></view>
-
-        
+        <Text>TU TIENDA</Text>
+        <Image style={styles.image} source={cartLogo}/>
       </View>
+
+
       <View style={styles.inputContainer}>
-        <View >
-          <TextInput
-            onChangeText={handleInputChange}
-            value={inputValue}
-            style={styles.input}
-            placeholder="Ingrese un producto"
-          />
-
-        </View>
-        <View style={{ backgroundColor:"#448FDA" }}>
-          <Pressable onPress={addItem}>
-            <Text style={{ fontSize: 30}}>+</Text>
-          </Pressable>
-        </View>
+        <TextInput
+          onChangeText={handleInputChange}
+          value={inputValue}
+          style={styles.input}
+          placeholder="Ingrese un producto"
+          
+        />
+        <Pressable onPress={addItem}>
+          <Text style={{ fontSize: 40 }}><Image style={styles.image1} source={lupa}/></Text>
+        </Pressable>
       </View>
+
+
       <View style={styles.productList}>
 
         <FlatList
           data={cartItems}
           renderItem={({ item }) => (
-            <View style={{width: 400}}>
+            <View style={{ width: 400, flexDirection: "row" }}>
               <Text style={styles.product}>{item.name}</Text>
+              <Pressable onPress={() => handleModal(item.id)}>
+                <Text style={{ fontSize: 20 }}><Image style={styles.image1} source={cruz}/></Text>
+              </Pressable>
             </View>
           )}
           keyExtractor={(item) => item.id}
         />
       </View>
-      <Pressable onPress={handleAddCounter}>
-        <Text style={{ fontSize: 20 }}>{counter}</Text>
-      </Pressable>
-      <Text>Valor del input: {inputValue}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#448FDA",
+    backgroundColor: "#DBA760",
     flex: 1,
     paddingHorizontal: 14,
-    gap:20
+    paddingTop: Constants.statusBarHeight,
+    
+  },
+  modalContainer: {
+    height: 200,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   header: {
+    backgroundColor: "#81ACDA",
     flexDirection: "row",
-    justifyContent: "space-evenly",
-    gap: 10,
+    justifyContent: "center",
+    gap: 8,
     alignItems: "center",
-    backgroundColor: "#F3EB90",
-    width:"100%"
+    border: "1px solid",
+
   },
   image: {
     width: 50,
     height: 50,
+  },
+  image1: {
+    width: 30,
+    height: 30,
   },
   productList: {
     justifyContent: "center",
@@ -125,12 +161,11 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 5,
     width: "90%",
-    backgroundColor: "#FDFFFC"
   },
   inputContainer: { 
     flexDirection: "row",
-    backgroundColor:"#448FDA",
-    width:"100%",
-    
- },
+    marginVertical: 10
+
+   },
 });
+
---
+++ src/global/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    orange1: "#FF7B54",
+    orange2: "#FFB26B",
+    orange3: "#FFD56F",
+}
+    
